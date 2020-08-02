7a25844d78a4da6ea0bf0836d9039884548b8fe8
sebastian <sebas0510.github.com>
2020-08-01 21:46:22 -0500

    Finalizamos el proyecto agregandole una función de javascript
    La función añade animaciones de salida al .content

---
+++ boletines/javascript/main.js
@@ -0,0 +1,22 @@
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link){
+
+  link.addEventListener("click", function(ev){
+    ev.preventDefault();
+    let content = document.querySelector('.content');
+
+    content.classList.remove("fadeInDown");
+    content.classList.remove("animated");
+
+    content.classList.add("fadeOutUp");
+    content.classList.add("animated");
+
+    setTimeout(function(){
+      location.href = "/";
+    },600);
+
+
+
+  });
+});
---
+++ laboratorio/main.js
@@ -0,0 +1,23 @@
+function empleado() {
+  var empleado = document.getElementById("empleado");
+  var formulario = empleado.innerHTML;
+  document.getElementById("demo").innerHTML = formulario;
+}
+
+function cliente() {
+  var cliente = document.getElementById("cliente");
+  var formulario = cliente.innerHTML;
+  document.getElementById("demo").innerHTML = formulario;
+}
+
+function auto(){
+  var auto = document.getElementById("auto");
+  var formulario = auto.innerHTML;
+  document.getElementById("demo").innerHTML = formulario;
+}
+
+function servicio(){
+  var servicio = document.getElementById("servicio");
+  var formulario = servicio.innerHTML;
+  document.getElementById("demo").innerHTML = formulario;
+}
